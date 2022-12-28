e98ffc02557f46faa571598fd142295cd7763ffb
Egor Sorokin <egor.v.s@mail.ru>
2022-12-28 23:47:12 +0300

    Task1 created

---
+++ Lesson1/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number1 = Prompt("Введите первое число: ");
+int number2 = Prompt("Введите второе число: ");
+
+if (number1 > number2) Console.WriteLine($"max = {number1}, min = {number2}");
+else if (number1 < number2)  Console.WriteLine("max = " + number2 + ", min = " + number1);
+else  Console.WriteLine("Числа равны");
