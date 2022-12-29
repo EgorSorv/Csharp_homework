288654676bd57a4a50613644efc82e52ee9d304d
Egor Sorokin <egor.v.s@mail.ru>
2022-12-29 11:06:59 +0300

    Task4 created

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = Prompt("Введите число: ");
+int counter = 1;
+
+if (number <= counter)
+{
+    while (number <= counter) number = Prompt("Число долно быть больше 1: ");
+    while (counter <= number)
+    {
+        if (counter % 2 == 0)
+        {
+            Console.Write($"{counter} ");
+            counter++;
+        }
+        else counter++;
+    }
+}
+else
+{
+    while (counter <= number)
+    {
+        if (counter % 2 == 0)
+        {
+            Console.Write($"{counter} ");
+            counter++;
+        }
+        else counter++;
+    }
+}
