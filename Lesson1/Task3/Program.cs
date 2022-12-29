3205f344247935fb6dee9d44184a4fc16ae82c4c
Egor Sorokin <egor.v.s@mail.ru>
2022-12-29 10:36:43 +0300

    Task3 created

---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = Prompt("Введите число: ");
+
+if (number % 2 == 0) Console.WriteLine($"{number} - это чётное число");
+else Console.WriteLine($"{number} - это нечётное число");
