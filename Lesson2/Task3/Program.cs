692b630be1a1d2139554b27aa47fc312353e1944
Egor Sorokin <egor.v.s@mail.ru>
2022-12-31 17:43:42 +0300

    Task3 added in Lesson2

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿int ReverseInt(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result = result * 10 + number % 10;
+        number /= 10;
+    }
+    return result;
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = Prompt("Введите число: ");
+
+if (number / 10 < 10) Console.WriteLine($"У числа {number} нет третьей цифры");
+else Console.WriteLine($"Третья цифра числа {number} - {ReverseInt(number) / 10 / 10 % 10}");
