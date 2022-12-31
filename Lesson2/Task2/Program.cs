0ec67fc07de205d8473a8cd08fc4cb10c6f111e9
Egor Sorokin <egor.v.s@mail.ru>
2022-12-31 16:23:20 +0300

    Task2 added in Lesson2

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿void FillArray(int[] array)
+{
+    array[0] = new Random().Next(1, 10);
+    int length = array.Length;
+    int index = 1;
+
+    while (index < 3)
+    {
+        array[index] = new Random().Next(0, 10);
+        index++;
+    }
+}
+
+int [] array = new int[3];
+FillArray(array);
+
+Console.WriteLine($"Сгенерировано число: {array[0]}{array[1]}{array[2]}");
+Console.WriteLine($"Преобразованное число: {array[0]}{array[2]}");
