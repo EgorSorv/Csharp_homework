2342be67e5397acec7817c34c4c964d0d4f79500
Egor Sorokin <egor.v.s@mail.ru>
2022-12-31 14:20:59 +0300

    Task1 added in Lesson2

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = Prompt("Введите положительное трехзначное число: ");
+while (number.ToString().Length != 3 || number < 0) number = Prompt("Число должно быть положительным и трехзначным: ");
+
+Console.WriteLine($"Втора цифра числа {number} - {number / 10 % 10}");
