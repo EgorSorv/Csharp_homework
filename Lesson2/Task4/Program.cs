924b931dd7754981eaa5db604325a1dc00d660fa
Egor Sorokin <egor.v.s@mail.ru>
2022-12-31 18:30:15 +0300

    Task4 added in Lesson2

---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int day = Prompt("Введите день недели: ");
+while (day > 7 || day < 1) day = Prompt("Такого дня недели не существует. Введите, пожалуйста, корректное значение: ");
+
+if (day == 6 || day == 7) Console.WriteLine("Это выходной день");
+else Console.WriteLine("Это рабочий день");
