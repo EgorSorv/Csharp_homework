316eab07ef6ade1de11a6128147f35e5f5c059bf
Egor Sorokin <egor.v.s@mail.ru>
2023-01-06 23:00:51 +0300

    Task1 created in Lesson3

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int ReverseInt(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result = result * 10 + number % 10;
+        number /= 10;
+    }
+    return result;
+}
+
+int number1 = Prompt("Введите число ");
+int number2 = ReverseInt(number1);
+
+if (number1 == number2) Console.WriteLine("Это число является палиндромом");
+else Console.WriteLine("Это число не является палиндромом");
