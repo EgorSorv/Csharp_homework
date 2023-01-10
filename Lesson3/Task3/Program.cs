cdd6d978f303e1cd7fb3fb4d1d5f1ca100f37828
Egor Sorokin <egor.v.s@mail.ru>
2023-01-10 22:13:05 +0300

    Task3 created in Lesson3

---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int N = Prompt("Введите число: ");
+Console.Write(1);
+
+for (int i = 2; i <= N; i++) Console.Write($", {Math.Pow(i, 3)}");
