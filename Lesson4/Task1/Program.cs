fa39f66736d4fece11c8bf807e91b1a7aec128f0
Egor Sorokin <egor.v.s@mail.ru>
2023-01-12 12:04:23 +0300

    Task1 added in Lesson4

---
+++ Lesson4/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+Clear();
+
+int Prompt(string message)
+{
+    Write(message);
+    int value = Convert.ToInt32(ReadLine());
+    return value;
+}
+
+double Exponentiation(int a, int b)
+{
+    return Math.Pow(a, b);
+}
+
+int number = Prompt("Введите число: ");
+int power = Prompt("Введите степень этого числа: ");
+
+WriteLine($"Число {number} в степени {power} => {Exponentiation(number, power)}");
