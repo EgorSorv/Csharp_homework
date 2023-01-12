a5a493c5f9a7c444adf60acf988d101213d963e5
Egor Sorokin <egor.v.s@mail.ru>
2023-01-12 22:55:06 +0300

    Task2 created in Lesson5

---
+++ Lesson5/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using static System.Console;
+using static System.String;
+
+Clear();
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(-20, 21);
+    return array;
+}
+
+void Print(int[] array, string separator = ",")
+{
+    string output = Join(separator, array);
+    WriteLine($"[{output}]");
+}
+
+int Prompt(string message)
+{
+    Write(message);
+    int value = Convert.ToInt32(ReadLine());
+    return value;
+}
+
+int SumOfNumbersOnUnevenPositions(int[] array)
+{   
+    int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 == 0) sum += array[i];
+    }
+
+    return sum;
+}
+
+int length = Prompt("Введите длину массива: ");
+int[] array = CreateArray(length);
+
+WriteLine("Содержание массива:");
+Print(array);
+
+WriteLine($"Сумма чисел, находящихся на нечетных позициях в массиве => {SumOfNumbersOnUnevenPositions(array)}");
