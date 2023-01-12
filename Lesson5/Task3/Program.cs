e1c2ab2b31357bc9ab80b976574d66c139860169
Egor Sorokin <egor.v.s@mail.ru>
2023-01-12 23:34:01 +0300

    Task3 created in Lesson5

---
+++ Lesson5/Task3/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+using static System.String;
+
+Clear();
+
+double[] CreateArray(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(0, 100) + new Random().NextDouble();
+    return array;
+}
+
+void Print(double[] array, string separator = ",")
+{
+    string output = Join(separator, array);
+    WriteLine($"[{output}]");
+}
+
+int Prompt(string message)
+{
+    Write(message);
+    int value = Convert.ToInt32(ReadLine());
+    return value;
+}
+
+double DifferenceBetweenMaxAndMin(double[] array)
+{   
+    double diff;
+    double max = array[0];
+    double min = array[0];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max)  max = array[i];
+        else if (array[i] < min) min = array[i];
+    }
+    diff = max - min;
+    return diff;
+}
+
+int length = Prompt("Введите длину массива: ");
+double[] array = CreateArray(length);
+
+WriteLine("Содержание массива:");
+Print(array);
+
+WriteLine($"Разница между максимальным и минимальным значением в массиве => {DifferenceBetweenMaxAndMin(array):F2}");
