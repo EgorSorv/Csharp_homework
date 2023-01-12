31f5bc2f3b10243dc795325f82b13a9006ccd5ea
Egor Sorokin <egor.v.s@mail.ru>
2023-01-12 12:20:30 +0300

    Task2 added in Lesson4

---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+Clear();
+
+int Prompt(string message)
+{
+    Write(message);
+    int value = Convert.ToInt32(ReadLine());
+    return value;
+}
+
+int SumOfElementsInNumber(int number)
+{
+    int sum = 0;
+
+    while (number != 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+
+    return sum;
+}
+
+int num = Prompt("Введите число: ");
+
+WriteLine($" Сумма цифр в числе {num} => {SumOfElementsInNumber(num)}");
