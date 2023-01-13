dbd784b2d2b0e5ccb151dd91d837ef85367bcf20
Egor Sorokin <egor.v.s@mail.ru>
2023-01-13 23:37:22 +0300

    Task1 added in Lesson6

---
+++ Lesson6/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+using static System.String;
+
+Clear();
+
+int Prompt(string message)
+{
+    Write(message);
+    int value = Convert.ToInt32(ReadLine());
+    return value;
+}
+
+int[] InputNumbers(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(ReadLine());
+    }
+    
+    return array;
+}
+
+int PositiveCounter(int[] array)
+{
+    int counter = 0;
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) counter++;
+    }
+
+    return counter;
+}
+
+int length = Prompt("Введите количество чисел: ");
+
+WriteLine();
+WriteLine("Введите эти числа:");
+WriteLine();
+
+int[] inputNumbers = InputNumbers(length);
+
+WriteLine($"Чисел больше 0 => {PositiveCounter(inputNumbers)}");
